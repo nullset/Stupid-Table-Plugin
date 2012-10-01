24431a372f177787fecd178ba9a3f0e849fefbdf
Nathan Wright <nathan@brandalism.com>
2012-10-01 14:18:19 -0600

    Added unregister click so that the function can be called on the same table more than once.

--- stupidtable.js
+++ stupidtable.js
@@ -1,3 +1,24 @@
+// Copyright (c) 2012 Joseph McCullough
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy   
+// of this software and associated documentation files (the "Software"), to deal   
+// in the Software without restriction, including without limitation the rights   
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell   
+// copies of the Software, and to permit persons to whom the Software is   
+// furnished to do so, subject to the following conditions:  
+// 
+// The above copyright notice and this permission notice shall be included in all  
+// copies or substantial portions of the Software.  
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR   
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE  
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER  
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  
+// SOFTWARE.
+
+
 // Stupid jQuery table plugin.
 
 // Call on a table 
@@ -75,7 +96,11 @@
     // ==================================================== //
     //                  Begin execution!                    //
     // ==================================================== //
-    // Do sorting when THs are clicked
+
+    // Unregister click handler, in case the sort method is called twice on the same table
+    table.undelegate(".table-data-header li", "click");
+
+    // Do sorting when header LIs are clicked
     table.delegate(".table-data-header li", "click", function(){
       var trs = table.find("ul.table-data-row");
       
